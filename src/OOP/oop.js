2f3ef14e3e4dae700b5e9a47e5f12ec8ede5526b
chykehyman <hyginuschinwoke@gmail.com>
2017-09-27 03:28:13 +0100

    create src directory and move 'OOP' and 'Test' folders in

---
+++ src/OOP/oop.js
@@ -0,0 +1,38 @@
+'use strict';
+
+export class Profession {
+    constructor(name) {
+            this._name = name;
+        }
+        // setter for name
+    set name(newName) {
+            this._name = newName;
+        }
+        // getter for name
+    get name() {
+        return this._name;
+    }
+
+}
+
+export class Soldier extends Profession {
+    // if this class is initialised without parameters, the default parameters are used
+    constructor(placeOfWork = 'Abuja', rank = 'Soldier', strength = "weak") {
+        super(name);
+        this._placeOfWork = placeOfWork;
+        this._rank = rank;
+        this._strengthLevel = strength;
+    }
+
+    getDetails() {
+        console.log(`Hello ${this._rank} ${this._name}. Your strength level is ${this._strengthLevel} and therefore, ${this.canFightBattle()}`);
+    }
+
+    canFightBattle() {
+        if (this._strengthLevel === "Very Strong") {
+            return "you are still fit for battle";
+        } else {
+            return "you are not fit for battle";
+        }
+    }
+}
